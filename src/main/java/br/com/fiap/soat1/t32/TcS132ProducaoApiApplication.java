4b68f0fd36928563dfbff4bbc59ef9eceb519cb4
silvester_silva <silvester_silva@hotmail.com>
2024-01-28 15:18:41 -0300

    estrtuta inicial

---
+++ src/main/java/br/com/fiap/soat1/t32/TcS132ProducaoApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.soat1.t32;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TcS132ProducaoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TcS132ProducaoApiApplication.class, args);
+	}
+
+}
